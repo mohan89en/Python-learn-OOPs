b9bb1cc9fab7a98d13f8252c1c0aff4f2742cfda
Mohan89en <mohangowdu630@gmail.com>
2022-11-15 21:14:50 +0530

    Circular linked list

--- DSA/circularll.py
+++ DSA/circularll.py
@@ -1,36 +1,37 @@
+
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.previous = None
-class CircularLinkedList:
-    def __init__(self):
-        self.head = None
+   def __init__(self, my_data):
+      self.data = my_data
+      self.next = None
 
-    def traverse(self, starting_point=None):
-        if starting_point is None:
-            starting_point = self.head
-        node = starting_point
-        while node is not None and (node.next != starting_point):
-            yield node
-            node = node.next
-        yield node
+class circularLinkedList:  
+   def __init__(self):
+      self.head = None
+   def add_data(self, my_data):
+      ptr_1 = Node(my_data)
+      temp = self.head    
+      ptr_1.next = self.head
 
-    def print_list(self, starting_point=None):
-        nodes = []
-        for node in self.traverse(starting_point):
-            nodes.append(str(node))
-        print(" -> ".join(nodes))
+      if self.head is not None:
+         while(temp.next != self.head):
+            temp = temp.next
+         temp.next = ptr_1
+      else:
+         ptr_1.next = ptr_1
+      self.head = ptr_1
 
-circular = CircularLinkedList()
-a = Node("a")
-b = Node("b")
-c = Node("c")
-d = Node("d")
-a.next = b
-b.next = c
-c.next = d
-d.next = a
-circular.head = a
-circular.print_list()
-CircularLinkedList.print_list()
+   def print_it(self):
+      temp = self.head
+      if self.head is not None:
+         while(True):
+            print("%d" %(temp.data)),
+            temp = temp.next
+            if (temp == self.head):
+               break
+my_list = circularLinkedList()
+print("Elements are added to the list ")
+my_list.add_data (56)
+my_list.add_data (78)
+my_list.add_data (12)
+print("The data is : ")
+my_list.print_it()
